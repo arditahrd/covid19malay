de3111c5cd27bd780222cb1e30c1cf9464dfbcf3
arditahrd <arditahardi15@gmail.com>
2020-07-29 00:00:52 +0700

    upgrade processing

--- final.js
+++ final.js
@@ -38,69 +38,101 @@ function updateData(id, negeri, lat, long, cases, date) {
     );
 }
 
-var count = 1;
 
-function fetchData() {
 
-    var interval = setInterval(function () {
+
+
+function getLatLong(address) {
+    var newArray = [];
+
+    for (var key in latLng) {
+
+        newArray.Push(key);
+
+    }
+    var lat = latLng[newArray[0]];
+    var lng = latLng[newArray[1]];
+    var geocoder = new google.maps.Geocoder();
+    var result = '';
+    geocoder.geocode({ 'address': address },
         request('https://www.map2uapps.com/server/rest/services/COVID19/COVID19_DailyHeatmap/FeatureServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22%3A-20037508.342788905%2C%22ymin%22%3A-60112525.02834715%2C%22xmax%22%3A20037508.342788905%2C%22ymax%22%3A20037508.342788905%2C%22spatialReference%22%3A{%22wkid%22%3A102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100&resultType=tile',
-            async function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-
-                    var data = JSON.parse(body);
-                    var total = data.features.length;
-                    console.log(total);
-
-                    var jumlah_case = 0;
-
-                    for (var n = 0; n < total; n++) {
-
-                        var kota = data.features[n].attributes.Negeri;
-                        var id = data.features[n].attributes.OBJECTID;
-                        var lat2 = data.features[n].attributes.Lat;
-                        var long = data.features[n].attributes.Long;
-                        var date = data.features[n].attributes.Date;
-                        var kasus = data.features[n].attributes.Cases;
-
-                        //get location
-                        const mapOptions = {
-                            provider: 'google',
-                            apiKey: 'AIzaSyBRAnJsQlQEKOgsCNfp_bmBKWsBbN1fEu0',
-                            formatter: null,
-                        };
-
-                        let address = [];
-                        const geocoder = NodeGeocoder(mapOptions);
-                        /*if (lat2 !== null && long !== null) {
-                            address = await geocoder.reverse({ lat: lat2, long: long });
-                        }*/
-
-                        geocoder.geocode('2.9264, 101.6964', function (err, res) {
-                            console.log(res);
-                            let data = JSON.stringify(res);
-                            fs.writeFileSync('WP Putrajaya.json', data);
-                        });
-
-                        //console.log(address);
-
-
-                        jumlah_case += kasus;
-                        if (count == 1) {
-                            //inputData(id, kota, lat, long, kasus, date);
-                        } else {
-                            //console.log(id + ' ' + kota + ' '+kasus);
-                            //updateData(id, kota, lat, long, kasus, date);
-                        }
-
-
-                    }
-                    //console.log("Jumlah kasus : " + jumlah_case);
-
-                    count = 4;
-
+            function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    result[lat] = results[0].geometry.location.lng();
+                    result[lng] = results[0].geometry.location.lng();
+                } else {
+                    result = 'Unable to find address: ' + status;
                 }
-            })
-    }, 5 * 1000);
+            }));
+    return result;
 }
 
-fetchData();
+getLatLong('address');
+
+
+// var count = 1;
+
+// function fetchData() {
+
+//     var interval = setInterval(function () {
+//         request('https://www.map2uapps.com/server/rest/services/COVID19/COVID19_DailyHeatmap/FeatureServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22%3A-20037508.342788905%2C%22ymin%22%3A-60112525.02834715%2C%22xmax%22%3A20037508.342788905%2C%22ymax%22%3A20037508.342788905%2C%22spatialReference%22%3A{%22wkid%22%3A102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100&resultType=tile',
+//             async function (error, response, body) {
+//                 if (!error && response.statusCode == 200) {
+
+//                     var data = JSON.parse(body);
+//                     var total = data.features.length;
+//                     console.log(total);
+
+//                     var jumlah_case = 0;
+
+//                     for (var n = 0; n < total; n++) {
+
+//                         var kota = data.features[n].attributes.Negeri;
+//                         var id = data.features[n].attributes.OBJECTID;
+//                         var lat2 = data.features[n].attributes.Lat;
+//                         var long = data.features[n].attributes.Long;
+//                         var date = data.features[n].attributes.Date;
+//                         var kasus = data.features[n].attributes.Cases;
+
+//                         //get location
+//                         const mapOptions = {
+//                             provider: 'google',
+//                             apiKey: 'AIzaSyBRAnJsQlQEKOgsCNfp_bmBKWsBbN1fEu0',
+//                             formatter: null,
+//                         };
+
+//                         let address = [];
+//                         const geocoder = NodeGeocoder(mapOptions);
+//                         /*if (lat2 !== null && long !== null) {
+//                             address = await geocoder.reverse({ lat: lat2, long: long });
+//                         }*/
+
+//                         geocoder.geocode('2.9264, 101.6964', function (err, res) {
+//                             console.log(res);
+//                             let data = JSON.stringify(res);
+//                             fs.writeFileSync('WP Putrajaya.json', data);
+//                         });
+
+//                         //console.log(address);
+
+
+//                         jumlah_case += kasus;
+//                         if (count == 1) {
+//                             //inputData(id, kota, lat, long, kasus, date);
+//                         } else {
+//                             //console.log(id + ' ' + kota + ' '+kasus);
+//                             //updateData(id, kota, lat, long, kasus, date);
+//                         }
+
+
+//                     }
+//                     //console.log("Jumlah kasus : " + jumlah_case);
+
+//                     count = 4;
+
+//                 }
+//             })
+//     }, 5 * 1000);
+// }
+
+// fetchData();
